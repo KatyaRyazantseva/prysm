3323750a01540de35d9b94e289ac3fb65360c092
terence tsao <terence@prysmaticlabs.com>
2024-10-03 08:09:21 -1000

    Use engine api get-blobs for block subscriber

--- beacon-chain/sync/subscriber_beacon_blocks.go
+++ beacon-chain/sync/subscriber_beacon_blocks.go
@@ -12,6 +12,7 @@ import (
 	"github.com/prysmaticlabs/prysm/v5/consensus-types/blocks"
 	"github.com/prysmaticlabs/prysm/v5/consensus-types/interfaces"
 	"github.com/prysmaticlabs/prysm/v5/io/file"
+	"github.com/prysmaticlabs/prysm/v5/time/slots"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -33,6 +34,8 @@ func (s *Service) beaconBlockSubscriber(ctx context.Context, msg proto.Message) 
 		return err
 	}
 
+	go s.attemptBlobSaveAndBroadcast(ctx, signed)
+
 	if err := s.cfg.chain.ReceiveBlock(ctx, signed, root, nil); err != nil {
 		if blockchain.IsInvalidBlock(err) {
 			r := blockchain.InvalidBlockRoot(err)
@@ -53,6 +56,56 @@ func (s *Service) beaconBlockSubscriber(ctx context.Context, msg proto.Message) 
 		return err
 	}
 	return err
+}
+
+// attemptBlobSaveAndBroadcast handles the process of saving and broadcasting blobs
+// by reconstructing blob sidecars from the execution layer (EL) and integrating them into the chain.
+func (s *Service) attemptBlobSaveAndBroadcast(ctx context.Context, block interfaces.ReadOnlySignedBeaconBlock) {
+	startTime, err := slots.ToTime(uint64(s.cfg.chain.GenesisTime().Unix()), block.Block().Slot())
+	if err != nil {
+		log.WithError(err).Error("Failed to convert slot to time")
+	}
+
+	blockRoot, err := block.Block().HashTreeRoot()
+	if err != nil {
+		log.WithError(err).Error("Failed to calculate block root")
+		return
+	}
+
+	// Reconstruct blob sidecars from the EL
+	blobSidecars, err := s.cfg.executionReconstructor.ReconstructBlobSidecars(ctx, block, blockRoot)
+	if err != nil {
+		log.WithError(err).Error("Failed to reconstruct blob sidecars")
+		return
+	}
+
+	// Get indices of already existing blobs in the database to avoid duplicates
+	indices, err := s.cfg.blobStorage.Indices(blockRoot)
+	if err != nil {
+		log.WithError(err).Error("Failed to retrieve indices for block")
+		return
+	}
+
+	// Iterate through the reconstructed sidecars, save them to the chain, and broadcast them to peers
+	for i, sidecar := range blobSidecars {
+		if indices[i] {
+			continue // Skip if the blob already exists in the database
+		}
+
+		if err := s.cfg.chain.ReceiveBlob(ctx, sidecar); err != nil {
+			log.WithError(err).Error("Failed to process blob")
+		}
+
+		if err := s.cfg.p2p.BroadcastBlob(ctx, sidecar.Index, sidecar.BlobSidecar); err != nil {
+			log.WithError(err).Error("Failed to broadcast blob")
+		}
+
+		blobRecoveredFromELCount.Inc()
+
+		fields := blobFields(sidecar.ROBlob)
+		fields["sinceSlotStartTime"] = s.cfg.clock.Now().Sub(startTime)
+		log.WithFields(fields).Debug("Successfully received and processed blob sidecar from EL")
+	}
 }
 
 // WriteInvalidBlockToDisk as a block ssz. Writes to temp directory.
